b848367c67e9583d00465dc816c0e3b1f6ed3fb4
Your Name <you@example.com>
2015-11-13 15:49:57 -0500

    Stuff 2

--- app.py
+++ app.py
@@ -10,15 +10,20 @@ def __isdigit(x):
 	except:
 		return False
 
-# Reset authcode page
+# Reset password
 @app.route('/auth/reset_password')
 def reset_password():
 	return render_template('reset-form.html')
 
-# Reset password page
-@app.route('/auth/reset_password')
+# Reset authcode
+@app.route('/auth/reset_authcode')
 def reset_authcode():
 	return render_template('reset-form.html')
+
+# Login page
+@app.route('/auth/login')
+def login():
+	return render_template('login.html')
 
 # Homepage
 @app.route('/')
